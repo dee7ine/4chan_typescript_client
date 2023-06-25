f2e54694ee18613d005d9b851ceac26d44856b32
dee7ine <dee7ine@gmail.com>
2023-06-25 20:08:08 +0200

    Minor changes to threads.ts documentation

--- src/threads.ts
+++ src/threads.ts
@@ -10,6 +10,18 @@ console.log(`https://a.4cdn.org/${board}/thread/${thread}.json`);
 // console.log(data)
 
 export async function getThread(board: string, thread: number | string){
+/**
+   * Returns the average of two numbers.
+   *
+   * @remarks
+   * Thread fetcher function
+   *
+   * @param x - board
+   * @param y - The second input number
+   * @returns The arithmetic mean of `x` and `y`
+   *
+   * @beta
+   */
     try {
         const response = await fetch(`https://a.4cdn.org/${board}/thread/${thread}.json`)
         if (!response.ok) {
@@ -29,7 +41,7 @@ export async function getThread(board: string, thread: number | string){
         }
 } 
 }
-
+// basic type for post info 
 export type BasicPost = {
     "no": number,
     "now": string,
@@ -47,7 +59,7 @@ export type BasicPost = {
     "fsize": number,
     "resto": number
 }
-
+// thread info wrapper 
 export type BasicThread = {
     data: BasicPost[];
 }
@@ -60,15 +72,14 @@ export class ThreadScrapper{
         this._board = board;
         this._thread = thread;
     };
-    
     /**
-     * Retrieves thread information json
-     * 
+     * Retrieves thread information as json
+     * @param {boolean} [log = false] - log to console
      *
      * @returns {Promise<BasicThread | object | string>}
      * @throws {Error}
      */
-    async getThread(log?: boolean): Promise<BasicThread |object | string> {
+    async getThread(log?: boolean): Promise<BasicThread | string> {
         try {
             const response = await fetch(`https://a.4cdn.org/${this._board}/thread/${this._thread}.json`)
             if (!response.ok) {
